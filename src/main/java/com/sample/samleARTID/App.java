896ef5a363d73307d4fadfdb9633ccb08e4541ce
prasad singamsetty <prasad.itengineeer@gmail.com>
2018-08-28 22:04:13 +0530

    test repo added

---
+++ src/main/java/com/sample/samleARTID/App.java
@@ -0,0 +1,13 @@
+package com.sample.samleARTID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
